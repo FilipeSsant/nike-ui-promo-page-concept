9ab06fb8e5233550882af9adb32fe71c29f0ef20
Filipe da Silva <filipe.ssant@outlook.com>
2021-07-18 14:18:33 -0300

    feat: add netfily image resource

--- src/components/Product/index.tsx
+++ src/components/Product/index.tsx
@@ -121,7 +121,10 @@ export function Product({ product }: ProductPageProps) {
                 }
               }}
             >
-              <img src={`/img/${actualVariant.img}`} alt={product.title} />
+              <img
+                src={`/img/${actualVariant.img}?nf_resize=fit`}
+                alt={product.title}
+              />
             </motion.div>
           </Styles.ProductImageContainer>
         </Styles.ProductInfoImageContainer>
